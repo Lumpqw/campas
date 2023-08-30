bb8dbe5ad3355bb20efc3ae2a380b89be53993f6
Lumpqw <131711531+Lumpqw@users.noreply.github.com>
2023-08-30 16:26:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+color = "red";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.strokeStyle();
+
+canvas.addEventListener("mousedown", myMousedown);
+
+function myMousedown(e)
+{
+    color = document.getElementById("color").ariaValueMax;
+    console.log(color);
+    mouseX = e.clientX - canvas.offsetLeft;
+    mouseY = e.clientY - canvas.offsetTop;
+    console.log("X = " + mouseX + " ,Y = " + mouseY);
+    circle(mouseX, MouseY);
+}
+
+function circle(mouseX, mouseY)
+{
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWitdh = 2;
+    ctx.arc(mouseX, mouseY, 40, 0, 2*Math.PI);
+    ctx.stroke();
+}
+
+function clearArea()
+{
+    ctx.clearRect(0, 0, canvas.witdh, canvas.height);
+}
